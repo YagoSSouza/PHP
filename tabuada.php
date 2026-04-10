6f8b54c36d3cfda6e7e2fc1944046f4530cdfd87
Yago Souza <124635178+YagoSSouza@users.noreply.github.com>
2026-04-10 14:08:09 -0300

    Update tabuada.php
    adding comments

--- tabuada.php
+++ tabuada.php
@@ -1,6 +1,7 @@
 <?php 
+//Entrada do usuário
 $numero = 5;
-
+//usamos o contador para percorrer de 1 a 10 incrementando +1 e então multiplicando
 for ($i=1; $i <= 10; $i++){
     $result = $numero*$i;
     echo ("$result\n");
