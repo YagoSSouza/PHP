621d08235d852b2b4f344075df815c6c8ce3010f
Yago Souza <124635178+YagoSSouza@users.noreply.github.com>
2026-04-10 14:06:01 -0300

    Update imc.php
    adding comments

--- imc.php
+++ imc.php
@@ -1,8 +1,10 @@
 <?php
+//Variavel recebe os valores do usuário
 $altura = 1.80;
 $peso = 80;
+//calculo imc
 $imc = $peso / ($altura**2);
-
+//condiona para a respectiva categoria
 if ($imc < 18.6) {
     echo ("Abaixo do peso\n");
     echo ("Imc: $imc");
